fcf76952fa52d3bcc5a64cc07f737872fcd9d98d
João Matheus Villarim <joao.matheus.almeida@ccc.ufcg.edu.br>
2023-09-21 17:46:27 -0300

    update o que esperar

---
+++ guia-th-ftm/oQueEsperar.js
@@ -0,0 +1,16 @@
+function cabelo() {
+    document.getElementById("info").innerHTML = "<b>Cabelo:</b> a textura do seu cabelo provavelmente irá mudar e a cor escurecer";
+}
+
+function rosto() {
+    document.getElementById("info").innerHTML = "<b>Rosto:</b> <ul> <li>seu rosto irá assumir uma forma mais masculina," +
+    " devido à redistribuição de gordura.</li><li>Pelos faciais irão começar a crescer em um ritmo lento, que pode" +
+    "ser acelerado com o uso de Minoxidil.</li><li>Os pelos das sobrancelhas irão engrossar e escurecer.</li><li>Sua voz irá" +
+    " engrossar, e há chance de desenvolver pomo de adão</li> </ul>";
+}
+
+function peito() {
+    document.getElementById("info").innerHTML = "<b>Peito e axilas:</b><ul><li>seu peito irá diminuir em tamanho, devido à redistribuição "+
+    "de gordura e escolhimento das glândulas mamárias.</li><li>crescerão pelos no peito, e os das axilas se tornarão mais escuros, longos "+
+    "e grossos.</li><li>você irá notar uma diferença nos seus odores corporais</li></ul>";
+}
